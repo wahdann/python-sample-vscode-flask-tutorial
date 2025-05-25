b6a48807364d4b8451ac450f34b34010990d86ce
Ahmed Wahdan <xwahdanx@gmail.com>
2025-05-25 16:32:39 +0300

    groovy script

--- jf.groovy
+++ jf.groovy
@@ -1,4 +1,7 @@
 node('newAgent') {
+    stage('Checkout Code') {
+        git url: 'https://github.com/wahdann/python-sample-vscode-flask-tutorial.git', branch: 'main'
+    }
     stage('Build Docker image') {
         sh "echo 'Building the image'"
         sh "docker build -t wahdann/cicdlab2:v${env.BUILD_NUMBER} ."
